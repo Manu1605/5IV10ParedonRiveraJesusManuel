dd7c96542c08a2613e25c8f58e120ff22d6c883b
Manu1605 <jparedonr2000@alumno.ipn.mx>
2022-09-23 17:37:26 -0500

    subiendo0203

---
+++ 03Viggenere/js/viggenere.js
@@ -0,0 +1,20 @@
+//Vamos a ocupar parte de cesar
+
+const abc= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+'m', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+'x', 'y', 'z'];
+
+//Llave
+
+let key="";
+
+//Interpretamos código
+//El $ sirve para mandar a llamar
+//Function() vacia se le llama función anónima
+
+$(document).ready(function(){
+    //Funcion para poder cifrar con viggenere
+    $('#ci').click(function(){
+        //Para cifrar vamos a utilizar una funcion de modulo la cual es y=(x+z)mod27
+    })
+})
