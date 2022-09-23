0d5b1af949d5fc86c3acb84c706a5f73e159e7fe
Manu1605 <jparedonr2000@alumno.ipn.mx>
2022-09-23 17:54:35 -0500

    subiendoto

--- 03Viggenere/js/viggenere.js
+++ 03Viggenere/js/viggenere.js
@@ -16,5 +16,47 @@ $(document).ready(function(){
     //Funcion para poder cifrar con viggenere
     $('#ci').click(function(){
         //Para cifrar vamos a utilizar una funcion de modulo la cual es y=(x+z)mod27
+
+        key=document.getElementById('llave').value;
+        //Debemos verificar la llave
+        key=key.replace(/ /g, '');
+
+        //Vamos a traer los datos del mensaje
+        let mess=document.getElementById('mess').value;
+        //Debemos verificar la llave
+        mess=mess.replace(/ /g, '');
+
+        let newMess="";
+        let keyCompleta="";
+
+        //Para aplicar el algoritmos debemos crear una función que se encargue de revisar las condiciones del mismo
+
+        if(revision(mess.key)){
+            //Vamos primero por aplicar y obtener la poscion de la longitud del mensaje y emparejarlo contra la llave
+
+            for(var i=0;i<mess.length;i++){
+                //Emparejo conforme a la posicion del caracter obteniendo el número de dicha posición
+                keyCompleta+=key.charAt((i%Number(key.length)));
+            }
+            alert(keyCompleta);
+
+            //Tengo que volver a recorrel el mensaje para obtener caracteres y posiciones
+
+            for(var i=0;i<mess.length;i++){
+                //Obtener la posicion de la letra
+                let charr=mess.charAt(i);
+
+                //Debemos crear una funcion para obtener la posicion del caracter
+                let posm=getPosicion(charr);
+
+                //Aplicarlo a la llave
+                charr=keyCompleta.charAt(i);
+
+                //Obtenemos la posicion
+                let posk=getPosicion(charr);
+            }
+
+        }
+
     })
 })
